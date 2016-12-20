8666cba30add500a386e70e47da7804dfc5fa8d1
Jérôme Macias <jerome.macias@gmail.com>
2016-12-20 23:32:02 +0100

    Add find and getAttribute driver helpers

--- src/driver/utils.js
+++ src/driver/utils.js
@@ -3,7 +3,9 @@ import { until } from 'selenium-webdriver';
 export default driver => ({
     waitForElementVisible: selector => driver.wait(until.elementLocated(selector)),
     getText: selector => driver.findElement(selector).getText(),
+    getAttribute: (selector, name) => driver.findElement(selector).getAttribute(name),
     getValue: selector => driver.findElement(selector).getAttribute('value'),
     setValue: (selector, value) => driver.findElement(selector).sendKeys(value),
     click: selector => driver.findElement(selector).click(),
+    find: selector => driver.findElement(selector),
 });
